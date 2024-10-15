36d663e4786e7a167b723701861f429d6bcc8a2e
taufik <tauf017@kominfo.go.id>
2024-10-16 01:36:10 +0700

    update compose

---
+++ build.sh
@@ -0,0 +1,9 @@
+cd app
+current_date=$(date +"%Y.%m.%d")
+tag="taufikp/monitoring-psre:${current_date}"
+echo $tag
+docker build . -t $tag
+docker push $tag
+tag2="taufikp/monitoring-psre:latest"
+docker build . -t $tag2
+docker push $tag2
